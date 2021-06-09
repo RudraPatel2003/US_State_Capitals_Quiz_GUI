6dd4afa3ca421fe322f97432e273aeed85cad8b2
Rudra Patel <patelrudra2003@gmail.com>
2021-06-08 20:44:28 -0500

    Adds Population Rank Web Scraper

---
+++ Population_Rank_Web_Scraper.py
@@ -0,0 +1,52 @@
+import requests
+from bs4 import BeautifulSoup
+from States_and_State_Capitals_Reader import list_of_state_names
+
+
+#the purpose of this file is to create a dictionary mapping different state names to their population rank
+#population ranks will be web scraped from a wikipedia table
+dictionary_of_population_ranks = {}
+
+
+#initialize request
+wikipedia_url = "https://en.wikipedia.org/wiki/List_of_states_and_territories_of_the_United_States_by_population"
+wikipedia_page_request = requests.get(wikipedia_url)
+wikipedia_page_text = wikipedia_page_request.text
+
+soup = BeautifulSoup(wikipedia_page_text, "html.parser")
+
+
+#find every table on the page
+all_tables = soup.find_all("table")
+
+
+#filter the tables to only those that contain captions
+tables_with_captions = [table for table in all_tables if table.caption is not None]
+
+
+#find the desired table, identified by its unique caption
+desired_table = None
+for table in tables_with_captions:
+    if (table.caption.string).strip() == "Population of states, territories, divisions and regions":
+        desired_table = table
+        break
+
+
+#find all of the rows in the desired table
+table_rows = desired_table.find_all("tr")
+
+
+#for each row, create a list containing each cell's content as a string
+for row in table_rows:
+    table_cells = row.find_all("td")
+    list_of_table_cells = [cell.text.strip() for cell in table_cells]
+
+    #only continue if the list is not empty
+    if len(list_of_table_cells) == 0:
+        continue
+
+    #if the first cell in the row is one of the 50 states, then extract the data needed to add a key-value pair to the dictionary
+    if list_of_table_cells[0] in list_of_state_names:
+        state = list_of_table_cells[0]
+        population_rank = int(list_of_table_cells[1])
+        dictionary_of_population_ranks[state] = population_rank
