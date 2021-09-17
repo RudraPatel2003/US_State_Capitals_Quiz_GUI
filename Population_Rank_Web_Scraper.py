9d50a959e76e85e49a06b91eb4d849205752d882
Rudra Patel <patelrudra2003@gmail.com>
2021-09-17 07:29:05 -0500

    Fixes web scraper after it broke

--- Population_Rank_Web_Scraper.py
+++ Population_Rank_Web_Scraper.py
@@ -19,21 +19,18 @@ soup = BeautifulSoup(wikipedia_page_text, "html.parser")
 #find every table on the page
 all_tables = soup.find_all("table")
 
-
 #filter the tables to only those that contain captions
 tables_with_captions = [table for table in all_tables if table.caption is not None]
+# print(len(tables_with_captions))
 
+#find the desired table
+#this step requires developer to manually determine which table to access 
+#in this case, there is only 1 table in the list to choose from
+#if there were more / if the page were to change, change the index 
+desire_tabled = tables_with_captions[0]
 
-#find the desired table, identified by its unique caption
-desired_table = None
-for table in tables_with_captions:
-    if (table.caption.string).strip() == "Population of states, territories, divisions and regions":
-        desired_table = table
-        break
-
-
-#find all of the rows in the desired table
-table_rows = desired_table.find_all("tr")
+#find all the rows in the table
+table_rows = desire_tabled.find_all("tr")
 
 
 #for each row, create a list containing each cell's content as a string
